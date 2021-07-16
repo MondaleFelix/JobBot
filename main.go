f6af20e379ee0edd1fe694a868caed79448fef2c
Mondale Felix <mondalefelix@gmail.com>
2021-07-16 08:57:38 -0700

    added interface and model

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func parseData() {
+
+}
+
+func broadcastData() {
+
+}
+
+func main() {
+	// Parses specific site or all sites
+	// Start each parsing of site in the separate goroutune
+	// Run broadcast parsed jobs from site
+
+	parseData()
+	broadcastData()
+
+}
